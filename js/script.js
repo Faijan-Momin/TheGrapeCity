8c691b79fe258711ac5499d4c6b68c8bca08a3a3
Faijan-Momin <faijanmomin99@gmail.com>
2021-04-08 22:30:51 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+// const swiper = new Swiper('.swiper-container', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    // Autoplay COntrol
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+//     // Autoplay COntrol
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
 
